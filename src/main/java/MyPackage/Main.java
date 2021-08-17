19346d6812f5532b49ad1b7c7a433890a980abfb
Aleksandr Kozlov <A362132@yandex.ru>
2021-08-17 21:25:10 +0300

    Create a string

---
+++ src/main/java/MyPackage/Main.java
@@ -0,0 +1,11 @@
+package MyPackage;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
